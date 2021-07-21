1f95ff85694e57803bc130ba93139fb80c2211bd
samyakshah3008 <samyakshah3008@gmail.com>
2021-07-21 23:13:57 +0530

    import js

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("script is working from different file")
+/* Input */
+var userName=prompt("Give me your userName")
+
+/* processing */
+var welcomeMessage = ("This script works!" + userName)
+
+/* output */
+
+alert(welcomeMessage)
