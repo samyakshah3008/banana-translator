364426cb6d4189b716f7e14535f6464480250172
samyakshah3008 <samyakshah3008@gmail.com>
2021-07-23 11:59:55 +0530

    css,js part done and added component lib for css

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
- function clickHandler() {
-   outputDiv.innerText = "abcdefg" + txtInput.value
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
 }
+
+function errorHandler(error) {
+  console.log("error occured",error);
+  alert("something is wrong with server! try again later")
+}
+
+ function clickHandler() {
+  // outputDiv.innerText = "abcdefg" + txtInput.value //
+
+  var inputText = txtInput.value;
+
+
+fetch(getTranslationURL(inputText))
+ .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated
+    outputDiv.innerText = translatedText
+  })
+   .catch(errorHandler)
+
+ };
+
 
 btnTranslate.addEventListener("click", clickHandler)
 
