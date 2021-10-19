b98a4b6867d9ab9bcedf49a31f697a023be57f3a
Samyak Shah <samyakshah3008@gmail.com>
2021-10-20 01:31:18 +0530

    REFACTOR: write clean code and add comments

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+// Linking from html world to javascript world 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
+// server url stored in placeholder
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+// adding event listener to button
+btnTranslate.addEventListener("click", clickHandler)
+
+function clickHandler() {
+var inputText = txtInput.value;
+outputDiv.innerText = "Translation in progress..." // message shown to client until we get translated value
+fetch(getTranslationURL(inputText))
+ .then(response => response.json())
+  .then(json => {
+    var translatedText = json.contents.translated
+    outputDiv.innerText = translatedText
+  })
+  // error handling
+   .catch(errorHandler) // calling a function
+
+ };
 
 function getTranslationURL(text) {
   return serverURL + "?" + "text=" + text
@@ -14,24 +33,6 @@ function errorHandler(error) {
   alert("something is wrong with server! try again later")
 }
 
- function clickHandler() {
-  // outputDiv.innerText = "abcdefg" + txtInput.value //
+ 
 
-  var inputText = txtInput.value;
-  
-  outputDiv.innerText = "Translation in progress..."
-
-
-fetch(getTranslationURL(inputText))
- .then(response => response.json())
-  .then(json => {
-    var translatedText = json.contents.translated
-    outputDiv.innerText = translatedText
-  })
-   .catch(errorHandler)
-
- };
-
-
-btnTranslate.addEventListener("click", clickHandler)
 
